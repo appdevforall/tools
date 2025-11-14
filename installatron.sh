90243f467fa400b010689db697cb1b887533297f
jimturner-adfa <jim.turner@appdevforall.org>
2025-11-14 13:59:02 -0800

    add code for appv8.apk (64bit) and appv7.apk (32bit) added code to show a alert on the phone when the installation succeeds

--- installatron.sh
+++ installatron.sh
@@ -5,24 +5,45 @@ SUCCESS="Success"
 ADB="adb"
 COGO="com.itsaky.androidide"
 COUNT=(0 0)
-TOD=$(date +'%Y-%m-%d,%H:%M:%S')
 
+## Define the function for installing the apk
+## Usage: install <apkname> (aka $1)
+## Args: <apkname> file name of the apk to be installed
+function install {
+    TOD=$(date +'%Y-%m-%d,%H:%M:%S')
+    echo "Installatroning $1 on $SERIAL model $MODEL for arch $ARCH"
+    result=$($ADB -s $SERIAL install -d -r  $1)
+    if [[ "$result" == *"$SUCCESS"* ]]; then
+	echo Successfully Installed $1
+	devnull=$(adb -s $SERIAL shell cmd notification post -S bigtext -t 'Alert' 'Installatron' "Installed\ new\ version\ of\ $1\ at\ $TOD")
+	status=0
+	((COUNT[0]++))
+    else
+	echo "Installation failed - $result", aborting current install
+	((COUNT[1]++))
+	status=-1
+    fi
+
+return $status
+}
+
+## Main
 echo "Installatron"
 for SERIAL in $($ADB devices | tail -n +2 | cut -sf 1);
 do
     ## use an ls here so that if the apk is not found, we get an empty list
     ##  as compared to the script crashing
-  for APKLIST in $(ls appRelease.apk);
-  do
+####  for APKLIST in $(ls appRelease.apk);
+####  do
       ## get the device's architecture, on v7 and v8 are supported
       ARCH=$($ADB -s $SERIAL shell getprop ro.product.cpu.abi)
       MODEL=$($ADB -s $SERIAL shell getprop ro.product.model)
       PKG=$($ADB -s $SERIAL shell pm list packages | grep -i $COGO)
       echo Turning off WiFi on device
       $ADB -s $SERIAL shell svc wifi disable
-      echo "Installatroning $APKLIST on $SERIAL model $MODEL for arch $ARCH"
       ## if Code On The Go already exists, unistall it
-      if [[ "$PKG" == "package$COGO" ]]; then
+      echo $PKG,,,$COGO
+      if [[ "$PKG" == *"$COGO"* ]]; then
 	  echo Uninstalling existing version
 	  result=$($ADB -s $SERIAL uninstall $COGO)
 	  if [[ "$result" == *"$SUCCESS"* ]]; then
@@ -35,24 +56,17 @@ do
       fi
       ## install the V8 or the v7 release version of Code On The Go
       if [[ "$ARCH" == arm64-v8a ]]; then
-	  result=$($ADB -s $SERIAL install -d -r  $APKLIST)
-	  if [[ "$result" == *"$SUCCESS"* ]]; then
-	      echo Successfully Installed $APKLIST
-	      adb -s $SERIAL shell cmd notification post -S bigtext -t 'Alert' 'Installatron' "Installed\ new\ version\ of\ $APKLIST\ on\ $TOD"
-	      ((COUNT[0]++))
-	  else
-	      echo "Installation failed - $result", aborting current install
-	      ((COUNT[1]++))
-	      continue
-	  fi
-      else 
+	  install appv8.apk
+      elif [[ "$ARCH" == armeabi-v7a ]]; then
+	  install appv7.apk
+      else
 	  echo unknown architecture - $ARCH, aborting current install
-	      ((COUNT[1]++))
+	  ((COUNT[1]++))
 	  continue
       fi
 
   done
 
-done
+#### done
 echo successfully installed on ${COUNT[0]} devices, falied to install on ${COUNT[1]} devices
 echo "Installatron has left the building"
